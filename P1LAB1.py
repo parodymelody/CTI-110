aead80b062c107bd6b3157a7c1f0d18eb4d14375
parodymelody <160150592+parodymelody@users.noreply.github.com>
2024-02-15 10:28:22 -0500

    Add files via upload
    First python assignment

---
+++ P1LAB1.py
@@ -0,0 +1,11 @@
+# Maurice Mccrimmon
+# 2/13/2024
+# P1LAB1
+# Using input and output
+
+#Create a variable and get input from user
+name= input("Enter your first name: ")
+
+#Output a message using the variable
+print("Hey", name, "\nWelcome to Zybooks!")
+#print("Welcome to zyBooks!")
